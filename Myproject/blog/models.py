c31799185c29fb6d5d5131cd4bb7131aca385a58
Mahdi-Nazari <mahdinazari830@gmail.com>
2021-01-21 00:13:17 +0330

    some changes in css, add categories, template tags, view for categories and etc

--- Myproject/blog/models.py
+++ Myproject/blog/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.utils import timezone
 from extensions.utils import jalali_converter
 
+# Create your manager here.
+class ArticleManager(models.Manager):
+    def published(self):
+        return self.filter(status = 'p')
+
+
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length = 200, verbose_name = "عنوان دسته بندی")
@@ -25,7 +31,7 @@ class Article(models.Model):
     
     title = models.CharField(max_length = 200, verbose_name = "عنوان مقاله")
     slug = models.SlugField(max_length = 100, unique = True, verbose_name = "آدرس مقاله")
-    category = models.ManyToManyField(Category, verbose_name = "دسته بندی")
+    category = models.ManyToManyField(Category, verbose_name = "دسته بندی", related_name = "articles")
     description = models.TextField(verbose_name = "توضیحات")
     thumbnail = models.ImageField(upload_to = "images", verbose_name = "تصویر مقاله")
     publish = models.DateTimeField(default = timezone.now, verbose_name = "زمان انتشار")
@@ -36,6 +42,7 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'مقاله'
         verbose_name_plural = 'مقالات'
+        ordering = ['-publish']
     
     def __str__(self):
         return self.title
@@ -43,3 +50,8 @@ class Article(models.Model):
     def jpublish(self):
         return jalali_converter(self.publish)
     jpublish.short_description = "زمان انتشار"
+    
+    def category_published(self):
+        return self.category.filter(status = True)
+    
+    objects = ArticleManager()
--- Myproject/blog/views.py
+++ Myproject/blog/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 #from django.http import HttpResponse, JsonResponse
-from .models import Article
+from .models import Article, Category
 
 # Create your views here.
 def home(request):
     context = {
-        "articles": Article.objects.filter(status = 'p').order_by('-publish')[:2]
+        "articles": Article.objects.published(),
     }
     return render(request, 'blog/home.html', context)
 
 def detail(request, slug):
     context = {
-        "article": get_object_or_404(Article, slug = slug, status = 'p')
+        "article": get_object_or_404(Article.objects.published(), slug = slug),
     }
     return render(request, 'blog/detail.html', context)
+
+def category(request, slug):
+    context = {
+        "category": get_object_or_404(Category, slug = slug, status = True)
+    }
+    return render(request, 'blog/category.html', context)
